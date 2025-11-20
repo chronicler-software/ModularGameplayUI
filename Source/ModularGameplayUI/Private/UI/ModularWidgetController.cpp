00f981f54f48fa6cb20c15e007d1ecbccad7ec92
Derek Reese <derek.reese@chroniclersoftware.com>
2025-11-19 22:25:20 -0800

    Adds a Modular Widget Controller for reading and writing to the UI

---
+++ Source/ModularGameplayUI/Private/UI/ModularWidgetController.cpp
@@ -0,0 +1,22 @@
+﻿// Copyright Chronicler.
+
+
+#include "UI/ModularWidgetController.h"
+
+void UModularWidgetController::SetWidgetControllerParams(const FWidgetControllerParams& WidgetControllerParams)
+{
+		PlayerController = WidgetControllerParams.PlayerController;
+		PlayerState = WidgetControllerParams.PlayerState;
+		AbilitySystemComponent = WidgetControllerParams.AbilitySystemComponent;
+		AttributeSet = WidgetControllerParams.AttributeSet;
+}
+
+void UModularWidgetController::BroadcastInitialValues()
+{
+	// Fill this out in child classes.
+}
+
+void UModularWidgetController::BindCallbacksToDependancies()
+{
+	// Fill this out in child classes.
+}
---
+++ Source/ModularGameplayUI/Public/UI/ModularWidgetController.h
@@ -0,0 +1,74 @@
+﻿// Copyright Chronicler.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+
+#include "ModularWidgetController.generated.h"
+
+class AModularPlayerController;
+class AModularPlayerState;
+class UModularAbilitySystemComponent;
+class UAttributeSet;
+
+
+USTRUCT(BlueprintType)
+struct FWidgetControllerParams
+{
+	GENERATED_BODY()
+	
+	FWidgetControllerParams() {}
+	FWidgetControllerParams(AModularPlayerController* PC,
+		AModularPlayerState* PS,
+		UModularAbilitySystemComponent* ASC,
+		const UAttributeSet* AS)
+	: PlayerController(PC), PlayerState(PS), AbilitySystemComponent(ASC), AttributeSet(AS) {};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<AModularPlayerController> PlayerController = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<AModularPlayerState> PlayerState = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UModularAbilitySystemComponent> AbilitySystemComponent = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<const UAttributeSet> AttributeSet = nullptr;
+};
+/**
+ * Base class for WidgetControllers. Contains a struct for WidgetControllerParams to tie into the
+ * ModularAbilitySystem. WidgetControllerParams returns a generic AttributeSet, in child classes,
+ * cast to the relevant AttributeSet class to bind to Attributes.
+ *
+ * This allows you to assign a controller for any widget that is tied directly into the Ability System.
+ * Works for both players and non-player characters.
+ */
+UCLASS()
+class MODULARGAMEPLAYUI_API UModularWidgetController : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	
+	UFUNCTION(BlueprintCallable)
+	virtual void SetWidgetControllerParams(const FWidgetControllerParams& WidgetControllerParams);
+	
+	virtual void BroadcastInitialValues();
+	virtual void BindCallbacksToDependancies();
+
+	protected:
+
+	UPROPERTY(BlueprintReadOnly, Category="WidgetController")
+	TObjectPtr<AModularPlayerController> PlayerController;
+
+	UPROPERTY(BlueprintReadOnly, Category="WidgetController")
+	TObjectPtr<AModularPlayerState> PlayerState;
+
+	UPROPERTY(BlueprintReadOnly, Category="WidgetController")
+	TObjectPtr<UModularAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY(BlueprintReadOnly, Category="WidgetController")
+	TObjectPtr<const UAttributeSet> AttributeSet;
+};
