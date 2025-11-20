076519e2dc0d2dedb23e1a87a7fd5bc1692b7b81
Derek Reese <derek.reese@chroniclersoftware.com>
2025-11-19 22:26:59 -0800

    Adds Layout support to the Modular Gameplay HUD

--- Source/ModularGameplayUI/Private/UI/ModularGameplayHUD.cpp
+++ Source/ModularGameplayUI/Private/UI/ModularGameplayHUD.cpp
@@ -4,19 +4,82 @@
 
 #include "AbilitySystemComponent.h"
 #include "AbilitySystemGlobals.h"
+#include "ModularAbilityPlayerState.h"
+#include "ActorComponent/ModularAbilitySystemComponent.h"
+
 #include "Async/TaskGraphInterfaces.h"
+
 #include "Components/GameFrameworkComponentManager.h"
+#include "UI/ModularGameplayLayout.h"
 #include "UObject/UObjectIterator.h"
+#include "TimerManager.h"
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(ModularGameplayHUD)
 
+
+class AModularAbilityPlayerState;
+class UAbilitySystemComponent;
+class UKulaPlayerAttributeSet;
+class AKulaPlayerState;
+class APlayerCharacter;
 class AActor;
 class UWorld;
 
 AModularGameplayHUD::AModularGameplayHUD(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
+	, ModularGameplayLayout(nullptr)
 {
 	PrimaryActorTick.bStartWithTickEnabled = false;
+}
+
+UModularWidgetController* AModularGameplayHUD::GetOverlayWidgetController(
+	const FWidgetControllerParams& WCParams)
+{
+	if (OverlayWidgetController == nullptr)
+	{
+		OverlayWidgetController = NewObject<UModularWidgetController>(this, OverlayWidgetControllerClass);
+		OverlayWidgetController->SetWidgetControllerParams(WCParams);
+		OverlayWidgetController->BindCallbacksToDependancies();
+		return OverlayWidgetController;
+	}
+	return OverlayWidgetController;
+}
+
+void AModularGameplayHUD::InitOverlay(AModularPlayerController* PC, AModularPlayerState* PS,
+	UModularAbilitySystemComponent* ASC, const UAttributeSet* AS)
+{
+	const FWidgetControllerParams WidgetControllerParams(PC, PS, ASC, AS);
+	UModularWidgetController* WidgetController = GetOverlayWidgetController(WidgetControllerParams);
+
+	ModularGameplayLayout = GetModularGameplayLayout();
+	if (ModularGameplayLayout == nullptr)
+	{
+		if (UWorld* World = GetWorld())
+		{
+			FTimerManager& TM = World->GetTimerManager();
+			if (!TM.IsTimerActive(OverlayInitRetryTimerHandle))
+			{
+				FTimerDelegate RetryDelegate;
+				RetryDelegate.BindUObject(this, &AModularGameplayHUD::InitOverlay, PC, PS, ASC, AS);
+				TM.SetTimer(OverlayInitRetryTimerHandle, RetryDelegate, 0.05f, true);
+			}
+		}
+		return;
+	}
+
+	// Layout is available, stop any pending retry timer
+	if (UWorld* World = GetWorld())
+	{
+		World->GetTimerManager().ClearTimer(OverlayInitRetryTimerHandle);
+	}
+
+	ModularGameplayLayout->SetWidgetController(WidgetController);
+	WidgetController->BroadcastInitialValues();
+}
+
+void AModularGameplayHUD::SetModularGameplayLayout(UModularGameplayLayout* InLayout)
+{
+	ModularGameplayLayout = InLayout;
 }
 
 void AModularGameplayHUD::PreInitializeComponents()
@@ -36,6 +99,12 @@ void AModularGameplayHUD::BeginPlay()
 void AModularGameplayHUD::EndPlay(const EEndPlayReason::Type EndPlayReason)
 {
 	UGameFrameworkComponentManager::RemoveGameFrameworkComponentReceiver(this);
+
+	// Ensure any pending retry timer is cleared on end play
+	if (UWorld* World = GetWorld())
+	{
+		World->GetTimerManager().ClearTimer(OverlayInitRetryTimerHandle);
+	}
 
 	Super::EndPlay(EndPlayReason);
 }
--- Source/ModularGameplayUI/Public/UI/ModularGameplayHUD.h
+++ Source/ModularGameplayUI/Public/UI/ModularGameplayHUD.h
@@ -2,14 +2,25 @@
 
 #pragma once
 
+#include "ModularLayoutInterface.h"
 #include "GameFramework/HUD.h"
-
+#include "ModularUserWidget.h"
+#include "ModularWidgetController.h"
 #include "ModularGameplayHUD.generated.h"
+
+
+class UAttributeSet;
+class UModularAbilitySystemComponent;
+class AModularPlayerState;
+class AModularPlayerController;
+struct FWidgetControllerParams;
 
 namespace EEndPlayReason { enum Type : int; }
 
 class AActor;
 class UObject;
+class UModularUserWidget;
+struct FTimerHandle;
 
 /**
  * AModularGameplayHUD
@@ -20,12 +31,31 @@ class UObject;
  * use an "Add Widget" action in your experience to add a HUD layout and widgets to it.
  */
 UCLASS(Config = Game)
-class AModularGameplayHUD : public AHUD
+class MODULARGAMEPLAYUI_API AModularGameplayHUD : public AHUD, public IModularLayoutInterface
 {
 	GENERATED_BODY()
 
 public:
-	AModularGameplayHUD(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+	explicit AModularGameplayHUD(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	UPROPERTY()
+	TObjectPtr<UModularUserWidget> OverlayWidget;
+	
+	UModularWidgetController* GetOverlayWidgetController(const FWidgetControllerParams& WCParams);
+
+	void InitOverlay(AModularPlayerController* PC, AModularPlayerState* PS, UModularAbilitySystemComponent* ASC,const UAttributeSet* AS);
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UModularUserWidget> OverlayWidgetClass;
+
+	UPROPERTY()
+	TObjectPtr<UModularWidgetController> OverlayWidgetController;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UModularWidgetController> OverlayWidgetControllerClass;
+
+	virtual UModularGameplayLayout* GetModularGameplayLayout() const override { return ModularGameplayLayout; }
+	virtual void SetModularGameplayLayout(UModularGameplayLayout* InLayout) override;
 
 protected:
 
@@ -41,4 +71,12 @@ protected:
 	//~AHUD interface
 	virtual void GetDebugActorList(TArray<AActor*>& InOutList) override;
 	//~End of AHUD interface
+
+	UPROPERTY()
+	UModularGameplayLayout* ModularGameplayLayout;
+
+private:
+	// Timer used to retry overlay initialization until ModularGameplayLayout becomes available
+	FTimerHandle OverlayInitRetryTimerHandle;
+	
 };
